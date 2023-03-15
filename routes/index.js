ef51536c9807469e2ffb3186a31957785c7966d3
Josh Hancock <livingicon@gmail.com>
2023-03-14 22:33:12 -0400

    Final commit

--- routes/index.js
+++ routes/index.js
@@ -15,8 +15,23 @@ const messages = [
 ];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Mini Messageboard', messages: messages });
 });
+
+router.get('/new', (req, res, next) => {
+  res.render('form');
+});
+
+// need router.post for new because the form submit buttons sends a post request
+router.post('/new', (req, res) => {
+  // accessing info from the form
+  const {message, user} = req.body;
+  // push it to the array
+  messages.push({ text: message, user: user, added: new Date() });
+  // send users back to the index page after submitting message
+  res.redirect('/');
+});
+
 
 module.exports = router;
